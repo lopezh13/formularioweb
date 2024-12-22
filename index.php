9610e3c5afbdb6b21e9346e292eed61f83f1f9d8
lopezh13 <lopezhinfo@gmail.com>
2024-12-22 12:01:27 -0600

    Corrigiendo texto en index.php

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ $registeredData = $controller->getRegisteredData();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulario Dinámico</title>
+    <title>Formulario Web</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
     <link rel="stylesheet" href="assets/css/styles.css">
